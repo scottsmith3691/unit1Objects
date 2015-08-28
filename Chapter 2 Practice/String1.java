c57e9fd79a4f10d7eb1219eb0c0c78346c8f438b
scottsmith3691 <spsmith@NN121-HA-11.HS.sd203.org>
2015-08-28 10:23:44 -0500

    TODAY

---
+++ Chapter 2 Practice/String1.java
@@ -0,0 +1,21 @@
+public class String1
+{
+    public static void main(String[] args)
+    {
+        String greeting= "Hello World!";
+        int n=greeting.length();
+        
+        System.out.println("Length of string: " + n);
+        
+        
+        
+        String river= "Mississippi";
+        String bigRiver= river.toUpperCase();
+        System.out.println("river: " + river + " Big river: " + bigRiver);
+        
+        
+        
+        
+    }
+}
+        
